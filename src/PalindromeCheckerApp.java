8dbfe5e2a9aa7776ae165717739b539b9217cbfd
Prasiddha6 <prasiddha0106@gmail.com>
2026-03-02 15:16:16 +0530

    UC7

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
-import java.util.Stack;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.Deque;
+import java.util.ArrayDeque;
  public class PalindromeCheckerApp {
      public static void main(String[] args) {
 
@@ -10,21 +9,21 @@ import java.util.LinkedList;
          System.out.print("Enter a string: ");
          String str = sc.nextLine();
 
-         Stack<Character> stack = new Stack<>();
-         Queue<Character> queue = new LinkedList<>();
+         Deque<Character> deque = new ArrayDeque<>();
 
-         // Push to Stack and Enqueue to Queue
+         // Insert characters into deque
          for (int i = 0; i < str.length(); i++) {
-             char ch = str.charAt(i);
-             stack.push(ch);      // LIFO
-             queue.add(ch);       // FIFO
+             deque.addLast(str.charAt(i));
          }
 
          boolean isPalindrome = true;
 
-         // Compare pop (stack) and dequeue (queue)
-         for (int i = 0; i < str.length(); i++) {
-             if (stack.pop() != queue.remove()) {
+         // Compare front and rear
+         while (deque.size() > 1) {
+             char front = deque.removeFirst();
+             char rear = deque.removeLast();
+
+             if (front != rear) {
                  isPalindrome = false;
                  break;
              }
