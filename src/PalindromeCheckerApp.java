6294f2cdd81603d46c681d5c218a636ab19ebed6
Prasiddha6 <prasiddha0106@gmail.com>
2026-03-02 15:12:31 +0530

    UC6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
  public class PalindromeCheckerApp {
      public static void main(String[] args) {
 
@@ -9,17 +11,20 @@ import java.util.Stack;
          String str = sc.nextLine();
 
          Stack<Character> stack = new Stack<>();
+         Queue<Character> queue = new LinkedList<>();
 
-         // Push characters into stack
+         // Push to Stack and Enqueue to Queue
          for (int i = 0; i < str.length(); i++) {
-             stack.push(str.charAt(i));
+             char ch = str.charAt(i);
+             stack.push(ch);      // LIFO
+             queue.add(ch);       // FIFO
          }
 
-         // Check palindrome
          boolean isPalindrome = true;
 
+         // Compare pop (stack) and dequeue (queue)
          for (int i = 0; i < str.length(); i++) {
-             if (str.charAt(i) != stack.pop()) {
+             if (stack.pop() != queue.remove()) {
                  isPalindrome = false;
                  break;
              }
